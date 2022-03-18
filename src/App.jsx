54921cc595d3a873c5d445114c94141c74686a74
Konsta Nenonen <konstanenonen@protonmail.com>
2022-03-18 23:18:54 +0200

    setInterval seems to cause trouble with react

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,34 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable react/jsx-one-expression-per-line */
 import React from 'react';
 
 function App() {
   const [timer, setTimer] = React.useState({
-    break: 5,
-    session: 25,
+    break: {
+      minutes: 5,
+      seconds: 0,
+    },
+    session: {
+      minutes: 25,
+      seconds: 0,
+    },
     current: 'Session',
+    timerID: 0,
   });
 
   function reset() {
     setTimer({
-      break: 5,
-      session: 25,
+      break: {
+        minutes: 5,
+        seconds: 0,
+      },
+      session: {
+        minutes: 25,
+        seconds: 0,
+      },
       current: 'Session',
+      timerID: 0,
     });
   }
 
@@ -20,7 +37,10 @@ function App() {
 
     setTimer((prevState) => ({
       ...prevState,
-      break: prevState.break - 1,
+      break: {
+        minutes: prevState.break.minutes - 1,
+        seconds: 0,
+      },
     }));
   }
 
@@ -28,7 +48,10 @@ function App() {
     if (timer.break >= 60) return;
     setTimer((prevState) => ({
       ...prevState,
-      break: prevState.break + 1,
+      break: {
+        minutes: prevState.break.minutes + 1,
+        seconds: 0,
+      },
     }));
   }
 
@@ -37,7 +60,10 @@ function App() {
 
     setTimer((prevState) => ({
       ...prevState,
-      session: prevState.session - 1,
+      session: {
+        minutes: prevState.session.minutes - 1,
+        seconds: 0,
+      },
     }));
   }
 
@@ -45,7 +71,38 @@ function App() {
     if (timer.session >= 60) return;
     setTimer((prevState) => ({
       ...prevState,
-      session: prevState.session + 1,
+      session: {
+        minutes: prevState.session.minutes + 1,
+        seconds: 0,
+      },
+    }));
+  }
+
+  function tickForward() {
+    setTimer((prevState) => {
+      if (prevState.session.seconds === 0) {
+        return ({
+          ...prevState,
+          session: {
+            minutes: prevState.session.minutes - 1,
+            seconds: 59,
+          },
+        });
+      }
+      return ({
+        ...prevState,
+        session: {
+          minutes: prevState.session.minutes,
+          seconds: prevState.session.seconds - 1,
+        },
+      });
+    });
+  }
+
+  function startTimer() {
+    setTimer((prevState) => ({
+      ...prevState,
+      timerID: setInterval(tickForward, 1000),
     }));
   }
 
@@ -57,7 +114,7 @@ function App() {
           <h2 id="break-label">Break Length</h2>
           <div className="controls-wrappers">
             <button id="break-increment" type="button" onClick={incrementBreak}>+1</button>
-            <div id="break-length">{timer.break}</div>
+            <div id="break-length">{timer.break.minutes}</div>
             <button id="break-decrement" type="button" onClick={decrementBreak}>-1</button>
           </div>
         </div>
@@ -65,14 +122,14 @@ function App() {
           <h2 id="session-label">Session Length</h2>
           <div className="controls-wrappers">
             <button id="session-increment" type="button" onClick={incrementSession}>+1</button>
-            <div id="session-length">{timer.session}</div>
+            <div id="session-length">{timer.session.minutes}</div>
             <button id="session-decrement" type="button" onClick={decrementSession}>-1</button>
           </div>
         </div>
         <div className="timer-wrapper">
           <h2 id="timer-label">{timer.current}</h2>
-          <h2 id="time-left">{timer.session}</h2>
-          <button id="start_stop" type="button">Start/Stop</button>
+          <h2 id="time-left">{timer.session.minutes}:{timer.session.seconds + (timer.session.seconds === 0 && '0')}</h2>
+          <button id="start_stop" type="button" onClick={startTimer}>Start/Stop</button>
           <button id="reset" type="button" onClick={reset}>Reset</button>
         </div>
       </div>
